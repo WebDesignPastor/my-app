b669d97b9159cb830c4a28cb70d47884a2cda508
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-04-11 12:27:08 +0200

    Added buttons to history

--- src/index.js
+++ src/index.js
@@ -74,6 +74,19 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Revenir au tour n°' + move :
+        'revenur au début de la partie';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+
     let status;
     if (winner) {
       status = winner + ' a gagné';
@@ -90,7 +103,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
